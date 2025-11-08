a285e5fb45875d0185894b9f5ed7b2d58212ae30
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-08 23:13:28 +0530

    allocate memory for 5 number dynamically increase to 8 number

---
+++ allocate-memory-for-5number-dynamically-increase-to-8-number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int *ptr;
+ptr = (int *) calloc(5,sizeof(int));
+printf("enter numbers(5)");
+for(int i=0; i<5;i++){
+scanf("%d",&ptr[i]);
+}
+ptr = realloc(ptr,8);
+printf("enter numbers(8)");
+for(int i=0; i<8;i++){
+scanf("%d",&ptr[i]);
+}
+for(int i=0; i<8;i++){
+printf("number %d is %d\n",i,ptr[i]);
+}
+return 0;
+}
