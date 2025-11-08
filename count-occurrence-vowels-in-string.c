f307ee1033986fd0475c31a4af39f14132e73f7b
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-09 00:04:10 +0530

    count occurrence vowels in string

---
+++ count-occurrence-vowels-in-string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ #include<stdio.h>
+int countVowels(char str[]);
+ int main(){
+char str[] = "rahulkumawat";
+printf("vowels are %d",countVowels(str));
+ }
+int countVowels(char str[]){
+int count = 0;
+for(int i=0;str[i] != '\0';i++){
+if(str[i] == 'a'||str[i] == 'e'||str[i] == 'i'||str[i] == 'o'||str[i] == 'u'){
+count++;
+}
+}
+return count;
+}
