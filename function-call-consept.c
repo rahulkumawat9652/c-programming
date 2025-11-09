01e3b1b581cf316efcf3b5b3659f94c8a827b814
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-09 07:57:45 +0530

    function call consept

---
+++ function-call-consept.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// declaration/prototype 
+void printHello();
+int main(){
+printHello();//function call
+return 0;
+}
+//function definition 
+void printHello(){
+    printf("Hello\n");
+    printf("RAHUL KUMAWAT");
+}
