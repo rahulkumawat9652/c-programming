cddba79ed21b6fd9051fd551bc3ba442d65907d8
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-09 07:53:06 +0530

    full name condition in string

---
+++ full-name-condition-in-string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void printString(char arr[]);
+int main(){
+char str[100];
+fgets(str,100,stdin);
+puts(str);
+return 0;
+}
+void printString(char arr[]){
+for(int i=0; arr[i] != '\0';i++){
+printf("%c", arr[i]);
+}
+printf("\n");
+}
