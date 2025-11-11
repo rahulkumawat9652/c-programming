210c12e36b64a149207b4978bdb0779e18a543b7
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-11 10:18:05 +0530

    used to create alias for data types

---
+++ used-to-create-alias-for-data-types.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+typedef struct student {
+int roll;
+float cgpa;
+char name[100];
+}stu;
+typedef struct computerengineeringstudent{
+int roll;
+float cgpa;
+char name[100];
+}coe;
+int main(){
+coe s1;
+s1.roll = 1664;
+s1.cgpa = 9.2;
+strcpy(s1.name,"rahul");
+printf("student name is %s\n", s1.name);
+return 0;
+}
