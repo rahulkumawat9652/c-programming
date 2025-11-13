d4091053facd13a4f4b84bdf77ad1807a0dfeabb
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-13 15:06:25 +0530

    string using student name rollnumber cgpa program concept

---
+++ string-using-student-name-rollnumber-cgpa-program-concept.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+struct student{
+int roll;
+float cgpa;
+char name[100];
+};
+int main(){
+struct student s1;
+s1.roll = 2580185;
+s1.cgpa = 9.8;
+strcpy(s1.name,"rahul kumawat");
+printf("student name = %s\n",s1.name);
+printf("student roll no = %d\n",s1.roll);
+printf("student cgpa = %f\n",s1.cgpa);
+ return 0;
+}
