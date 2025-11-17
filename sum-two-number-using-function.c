84121b9298a16456d0ecf3ec08dd75c6f5b80446
RAHUL KUMAWAT <rahulkumawat9652@gmail.com>
2025-11-17 14:36:18 +0530

    sum two number using function

--- sum-two-number-using-function.c
+++ sum-two-number-using-function.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main(){
-    int a,b;
-    printf("enter number a");
-    scanf("%d",&a);
-    printf("enter number b");
-    scanf("%d",&b);
-    int S = sum(a,b);
-    printf("sum is %d",S);
-    return 0;
+int a,b;
+printf("enter number a");
+scanf("%d",&a);
+printf("enter number b");
+scanf("%d",&b);
+int S = sum(a,b);
+printf("sum is %d",S);
+return 0;
 }
 int sum(int x,int y){
 return x+y;
