8e56a4b46517bad53add085670012da501b2a9a5
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-17 14:31:50 +0530

    sum two number using gunction

---
+++ sum-two-number-using-function.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter number a");
+    scanf("%d",&a);
+    printf("enter number b");
+    scanf("%d",&b);
+    int S = sum(a,b);
+    printf("sum is %d",S);
+    return 0;
+}
+int sum(int x,int y){
+return x+y;
+}
