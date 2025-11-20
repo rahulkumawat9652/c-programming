ba0c81d757b44fcae5280a12487e03a232963ab7
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-11-20 16:02:11 +0530

    interest concept

---
+++ interest-consept.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+float principal,rate,time,si;
+printf("enter principal");
+scanf("%f",&principal);
+printf("enter rate");
+scanf("%f",&rate);
+printf("enter time");
+scanf("%f",&time);
+si = (principal*rate*time)/100;
+printf("your simple interest is %f",si);
+return 0; 
+}
