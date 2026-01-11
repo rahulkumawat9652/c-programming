2ea337cffb46fc8b641e708ee3068ca0f13dbc79
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-01-11 12:32:34 +0530

    matrix sum program

---
+++ matrix-sum-progarm.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+int i,j;
+int a[2][2], b[2][2], sum[2][2];
+printf("Enter number of rows and columns matrix 1 : ");
+for(int i = 0; i < 2; i++) {
+for(int j = 0; j < 2; j++) {
+scanf("%d", &a[i][j]);
+}
+}
+printf("Enter elements of second matrix:\n");
+for(int i = 0; i < 2; i++) {
+for(int j = 0; j < 2; j++) {
+scanf("%d", &b[i][j]);
+}
+}
+// Matrix Sum
+for(int i = 0; i < 2; i++) {
+for(int j = 0; j < 2; j++) {
+sum[i][j] = a[i][j] + b[i][j];
+}
+}
+printf("Sum of matrices:\n");
+for(int i = 0; i < 2; i++) {
+for(int j = 0; j < 2; j++) {
+printf("%d ", sum[i][j]);
+}
+printf("\n");
+}
+return 0;
+}
